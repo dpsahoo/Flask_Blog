a05c819f369d13c1abd254c94df5d312f2367e5b
Durga Sahoo <dpsahoo@gmail.com>
2019-07-29 01:31:54 +1000

    Modify routes to render templates

--- app.py
+++ app.py
@@ -1,17 +1,32 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
+
+posts = [
+    {
+        'author': 'Durga Sahoo',
+        'title': 'Blog Post 1',
+        'content': 'First post content...',
+        'date_posted': 'Jul 21, 2019'
+    },
+    {
+        'author': 'Deepu S',
+        'title': 'Blog Post 2',
+        'content': 'Second post content...',
+        'date_posted': 'Jul 22, 2019'
+    }
+]
 
 
 @app.route("/")
 @app.route("/home")
 def hello_world():
-    return "<h1>Home Page</h1>"
+    return render_template('home.html', posts=posts)
 
 
 @app.route("/about")
 def about():
-    return "<h1>About Page</h1>"
+    return render_template('about.html', title='About')
 
 
 if __name__ == '__main__':
