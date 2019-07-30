b22244a46afd2a1e133fd9d6239e67fa1112596a
Durga Sahoo <dpsahoo@gmail.com>
2019-07-31 01:24:59 +1000

    User Registration - Password Hashing 1

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -1,10 +1,12 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'c8ca7a149fbc2248095f13968c84d6bd'   # string manually generated from secrets.hex_token(16)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
+bcrypt = Bcrypt()
 
 
 from flaskblog import routes
