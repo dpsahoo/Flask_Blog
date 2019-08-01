230bb39a0b9715aed674ea9f613e2cc71e7ac0ce
Durga Sahoo <dpsahoo@gmail.com>
2019-08-01 19:45:55 +1000

    Username and Email validation in Registration process

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'c8ca7a149fbc2248095f13968c84d6bd'   # string manually generated from secrets.hex_token(16)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
-bcrypt = Bcrypt()
+bcrypt = Bcrypt(app)
 
 
 from flaskblog import routes
